531060b7af58492894875c53d9351344057b3fd6
Michael <hirsch617@gmail.com>
2015-04-24 15:38:22 -0500

    six

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ config = {
     'url': 'github.com/jswoboda/ISRSpectrum',
     'author_email': 'swoboj@bu.edu',
     'version': '1.0',
-    'install_requires': ['numpy', 'scipy', 'tables','numba'],
+    'install_requires': ['six','numpy', 'scipy', 'tables','numba'],
     'packages': ['ISRSpectrum'],
     'scripts': [],
     'name': 'ISRSpectrum'

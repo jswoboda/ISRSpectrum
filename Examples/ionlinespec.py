e694631d4f9a3f56f3cc38f732bffaffd967ece5
John Swoboda <swoboj@bu.edu>
2016-11-23 18:17:25 -0500

    added acf stuff

--- Examples/ionlinespec.py
+++ Examples/ionlinespec.py
@@ -5,6 +5,7 @@ Created on Mon Aug 29 16:29:42 2016
 @author: John Swoboda
 """
 import numpy as np
+import scipy.fftpack as scfft
 import os,inspect
 from ISRSpectrum.ISRSpectrum import ISRSpectrum
 import matplotlib.pylab as plt
@@ -18,16 +19,20 @@ if __name__== '__main__':
     imagepath = os.path.join(os.path.split(curpath)[0],'Doc','Figs')
     databloc = np.array([[1e11,1e3],[1e11,2.5e3]])
     nspec=256
-    ISpec_ion = ISRSpectrum(centerFrequency = 449e6, nspec=nspec, sampfreq=50e3,dFlag=True)
+    spfreq=50e3
+    ISpec_ion = ISRSpectrum(centerFrequency = 449e6, nspec=nspec, sampfreq=spfreq,dFlag=True)
     species=['O+','e-']
 #    databloc = np.array([[1.66e10,863.],[1.66e10,863.]])
     ylim=[0,1.4]
-    
+    ylim2=[-.5,1.4]
     flims=np.array([3.03e6,3.034e6])
     #%% With B-Field
     
     
     fion,ionline= ISpec_ion.getspecsep(databloc,species)
+    
+    acf=scfft.ifft(scfft.ifftshift(ionline)).real
+    tau=scfft.ifftshift(np.arange(-np.ceil((float(nspec)-1)/2),np.floor((float(nspec)-1)/2)+1))/spfreq
     
     
     
@@ -47,8 +52,23 @@ if __name__== '__main__':
     ax.set_ylim(ylim)
     plt.savefig(os.path.join(imagepath,'Specion.png'),dpi=300)
     
+    
+    fig,ax = plt.subplots(1,1,sharey=True, figsize=(6,4),facecolor='w')
+    
+    l1=ax.plot(tau[:64]*1e6,acf[:64]/acf[0],'-',lw=3)[0]
+    sns.despine()
+
+    ax.set_xlim([0,280])
+    ax.spines['right'].set_visible(False)
+
+    ax.set_xlabel(r'$\tau$ in $\mu$s ',fontsize=14)
+    ax.set_title(r'$\langle|n_e(|\mathbf{k}|=18.5,\tau)|^2\rangle$',fontsize=18)
+    ax.set_ylabel(r'Normalized Magnitude',fontsize=14)
+    plt.tight_layout()
+    ax.set_ylim(ylim2)
+    plt.savefig(os.path.join(imagepath,'acfion.png'),dpi=300)
     #%% With random values
-    n_pulse=np.array([50,200,500,1000])
+    n_pulse=np.array([1,10,50,200])
     lab_strs=['J={0}'.format(int(i))for i in n_pulse]
     lab_strs.insert(0,'Original')
     np_1=n_pulse.max()
@@ -57,9 +77,10 @@ if __name__== '__main__':
     filt=np.tile(np.sqrt(ionline[np.newaxis]),(np_1,1)).astype(x.dtype)
     y=x*filt
     ysqrt=y.real**2+y.imag**2
-    
+    ysqrtfft=scfft.ifft(scfft.ifftshift(ysqrt,axes=-1),axis=-1)
     ystats=np.array([ysqrt[:i].mean(axis=0) for i in n_pulse])
-    
+    #ystatsacf=scfft.ifft(scfft.ifftshift(ystats,axes=-1),axis=-1).real
+    ystatsacf=np.array([ysqrtfft[:i].mean(axis=0).real for i in n_pulse])
     fig,ax = plt.subplots(1,1,sharey=True, figsize=(6,4),facecolor='w')
     
     l1=ax.plot(fion*1e-3,ionline/ionline.max(),'-',lw=3,zorder=len(n_pulse))[0]
@@ -78,3 +99,24 @@ if __name__== '__main__':
     plt.tight_layout()
     ax.legend(hand,lab_strs,)
     plt.savefig(os.path.join(imagepath,'Specionave.png'),dpi=300)
+    
+    
+    
+    fig,ax = plt.subplots(1,1,sharey=True, figsize=(6,4),facecolor='w')
+    l1=ax.plot(tau[:64]*1e6,acf[:64]/acf[0],'-',lw=3,zorder=len(n_pulse))[0]
+    hand=[l1]
+    for iyn,iy in enumerate(ystatsacf):
+        l1=ax.plot(tau[:64]*1e6,iy[:64]/acf[0],'-',lw=3,zorder=iyn)[0]
+        hand.append(l1)
+    sns.despine()
+
+    ax.set_xlim([0,280])
+    ax.spines['right'].set_visible(False)
+
+    ax.set_xlabel(r'$\tau$ in $\mu$s ',fontsize=14)
+    ax.set_title(r'ACF Averaging',fontsize=18)
+    ax.set_ylabel(r'Normalized Magnitude',fontsize=14)
+    plt.tight_layout()
+    ax.set_ylim(ylim2)
+    ax.legend(hand,lab_strs,)
+    plt.savefig(os.path.join(imagepath,'acfionave.png'),dpi=300)

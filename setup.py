5271d1ba8e303e09e3469d11f1ca510a6aa595ca
John Swoboda <swoboj@bu.edu>
2020-06-16 18:29:55 -0400

    bumped version to 3.0.1

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ config = dict(
     install_requires=req,
     python_requires='>=3',
     extras_require={'plot':['matplotlib','jupyter','seaborn'],},
-    version='3.0.0',
+    version='3.0.1',
     packages= find_packages(),
     name= 'ISRSpectrum')
 

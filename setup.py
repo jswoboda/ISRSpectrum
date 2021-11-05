6f1cb407a7b3288fbe1f1e21f22b7e2d728d05dd
John Swoboda <swoboj@bu.edu>
2021-11-05 16:36:06 -0400

    bumped version to 3.0.2

--- setup.py
+++ setup.py
@@ -5,24 +5,28 @@ This is the setup file for the RadarDataSim python package
 
 @author: John Swoboda
 """
-req = ['nose','six','numpy','scipy','pandas','seaborn']
+req = ["nose", "six", "numpy", "scipy", "pandas", "seaborn"]
 
 import os
-from setuptools import setup,find_packages
+from setuptools import setup, find_packages
 
 config = dict(
-    description='Creates ISR Spectrums',
-    author='John Swoboda',
-    url='https://github.com/jswoboda/ISRSpectrum',
+    description="Creates ISR Spectrums",
+    author="John Swoboda",
+    url="https://github.com/jswoboda/ISRSpectrum",
     install_requires=req,
-    python_requires='>=3',
-    extras_require={'plot':['matplotlib','jupyter','seaborn'],},
-    version='3.0.1',
-    packages= find_packages(),
-    name= 'ISRSpectrum')
+    python_requires=">=3",
+    extras_require={
+        "plot": ["matplotlib", "jupyter", "seaborn"],
+    },
+    version='3.0.2',
+    packages=find_packages(),
+    name="ISRSpectrum",
+    package_data={'ISRSpectrum': ['ISRSpectrum/*.csv']}
+)
 
 curpath = os.path.dirname(__file__)
-testpath = os.path.join(curpath,'Test')
+testpath = os.path.join(curpath, "Test")
 try:
     os.mkdir(testpath)
     print("created {}".format(testpath))

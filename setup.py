f109d24e8113f80f6ed236f8fc65e2d1e25338b1
John Swoboda <swoboj@bu.edu>
2024-06-28 16:36:05 -0400

    bumped version to 4.0.7

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ config = dict(
     extras_require={
         "plot": ["matplotlib", "jupyter", "seaborn"],
     },
-    version='4.0.6',
+    version='4.0.7',
     cmdclass=versioneer.get_cmdclass(),
     packages=find_packages(),
     name="ISRSpectrum",

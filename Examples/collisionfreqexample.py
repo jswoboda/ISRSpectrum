Fixed issue with collisions and numerical integration

Examples/collisionfreqexample.py
@@ -4,57 +4,136 @@ Created on Thu Nov 12 10:51:26 2015
 
 @author: John Swoboda
 """
+
 from pathlib import Path
+
 import matplotlib.pylab as plt
-import seaborn as sns
 import numpy as np
+import scipy.constants as spconst
+import seaborn as sns
+from pyparsing.common import OneOrMore
+from seaborn._core.typing import DataSource
+
 #
-from ISRSpectrum import Specinit
+from ISRSpectrum import INFODICT, Specinit, get_collisionfreqs
+from ISRSpectrum.plugins.gord_default import collacf, magacf, magncollacf
 
 
 def main():
     sns.set_style("whitegrid")
     sns.set_context("notebook")
-    n_species=['H', 'He','N','N2', 'O', 'O2']
-    Nn = np.array([22340974.0, 1.170203e+08, 269365.12, 8.7995607e+12,5.6790339e+11,1.9389578e+12])*1e6
+    n_species = ["H", "He", "N", "N2", "O", "O2"]
+    Nn = (
+        np.array(
+            [
+                22340974.0,
+                1.170203e08,
+                269365.12,
+                8.7995607e12,
+                5.6790339e11,
+                1.9389578e12,
+            ]
+        )
+        * 1e6
+    )
     Tn = 179.29138
-    n_datablock = np.zeros((len(Nn),2))
-    n_datablock[:,0] = Nn
-    n_datablock[:,1] = Tn
+    n_datablock = np.zeros((len(Nn), 2))
+    n_datablock[:, 0] = Nn
+    n_datablock[:, 1] = Tn
 
-
-    species = ['NO+','O2+','e-']
-    Ni = np.array([1998.2988800000001, 57.700355999999999])*1e6
+    species = ["NO+", "O2+", "e-"]
+    Ni = np.array([1998.2988800000001, 57.700355999999999]) * 1e6
     Ti = 175.1
-    Ne= 2055.9992320000001 *1e6
+    Ne = 2055.9992320000001 * 1e6
     Te = 175.1
-    datablock = np.zeros((len(Ni)+1,2))
-    datablock[:-1,0] = Ni
-    datablock[:-1,1] = Ti
-    datablock[-1,0] = Ne
-    datablock[-1,1] = Te
+    datablock = np.zeros((len(Ni) + 1, 2))
+    datablock[:-1, 0] = Ni
+    datablock[:-1, 1] = Ti
+    datablock[-1, 0] = Ne
+    datablock[-1, 1] = Te
 
-    #
-    ISS1 = Specinit(centerFrequency = 449e6, bMag = 0.4e-4, nspec=512, sampfreq=25e3,dFlag=True)
+    nuparr, nuperp, nuneu = get_collisionfreqs(
+        datablock, species, n_datablock=n_datablock, n_species=n_species
+    )
+    cent_freq = 449e6
+    bMag = 4e-4
+    K = 2.0 * np.pi * 2 * cent_freq / spconst.c
 
-    (omeg,spec_1)=ISS1.getspecsep(datablock,species,vel = 0.0,rcsflag=False)
-    (omeg,spec_2)=ISS1.getspecsep(datablock,species,vel = 0.0,rcsflag=False,col_calc = True)
-    (omeg,spec_3)=ISS1.getspecsep(datablock,species,vel = 0.0,rcsflag=False,col_calc = True,n_datablock=n_datablock,n_species=n_species)
+    print(f"Parallel collisions: {nuparr}")
+    print(f"Perpendicular collisions: {nuperp}")
+    print(f"Neutral collisions: {nuneu}")
+    m_list = np.array([INFODICT[isp][-1] for isp in species]).astype(float)
+    m_ar = np.empty_like(m_list, dtype=float)
+    m_ar[:-1] = m_list[:-1] * spconst.m_p
+    m_ar[-1] = spconst.m_e
+    C_vec = np.sqrt(spconst.k * datablock[:, -1] / m_ar)
+    onorm = K * C_vec * np.sqrt(2)
 
+    Om_vec = spconst.e * bMag / m_ar / onorm
+    nuparr_n = nuparr / onorm
+    nuperp_n = nuperp / onorm
+    nuneu_n = nuneu / onorm
+    n_pnts = int(2**12)
+    dtau = 2e-3
+    tau = np.arange(n_pnts) * dtau
+    gordnun = np.empty((3, n_pnts), dtype=float)
+    for inum in range(len(species)):
+        gordnun[inum, :] = magncollacf(
+            tau,
+            np.pi / 2,
+            Om_vec[inum],
+            nuparr_n[inum],
+            nuperp_n[inum],
+            nuneu_n[inum],
+        )
+    fig, ax = plt.subplots(1, 2, figsize=(8, 4))
+    for inum, igord in enumerate(gordnun):
+        ax[0].plot(tau, igord, label=species[inum])
+        ax[1].plot(tau, igord, label=species[inum])
+    ax[0].plot(tau, np.exp(-(tau**2)), label="simp")
+    ax[1].plot(tau, np.exp(-(tau**2)), label="simp")
+    ax[1].set_yscale("log")
+    ISS1 = Specinit(
+        centerFrequency=cent_freq, bMag=0.4e-4, nspec=512, sampfreq=25e3, dFlag=True
+    )
 
-    (figmplf, axmat) = plt.subplots(1, 1,figsize=(20, 15), facecolor='w')
+    (omeg, spec_1) = ISS1.getspecsep(datablock, species, vel=0.0, rcsflag=False)
+    (omeg, spec_2) = ISS1.getspecsep(
+        datablock, species, vel=0.0, rcsflag=False, col_calc=True
+    )
+    (omeg, spec_3) = ISS1.getspecsep(
+        datablock,
+        species,
+        vel=0.0,
+        rcsflag=False,
+        col_calc=True,
+        n_datablock=n_datablock,
+        n_species=n_species,
+    )
+
+    (figmplf, axmat) = plt.subplots(1, 1, figsize=(20, 15), facecolor="w")
     curax = axmat
-    lines = [None]*3
-    labels = ['Spectrum','Spectrum with Coulomb Collisions','Spectrum with Columb and Neutral Collisions']
-    lines[0] = curax.plot(omeg*1e-3,spec_1/np.max(spec_1),label='Output',linewidth=5)[0]
-    lines[1] = curax.plot(omeg*1e-3,spec_2/np.max(spec_2),label='Output',linewidth=5)[0]
-    lines[2] = curax.plot(omeg*1e-3,spec_3/np.max(spec_3),label='Output',linewidth=5)[0]
+    lines = [None] * 3
+    labels = [
+        "Spectrum",
+        "Spectrum with Coulomb Collisions",
+        "Spectrum with Columb and Neutral Collisions",
+    ]
+    lines[0] = curax.plot(
+        omeg * 1e-3, spec_1 / np.max(spec_1), label="Output", linewidth=5
+    )[0]
+    lines[1] = curax.plot(
+        omeg * 1e-3, spec_2 / np.max(spec_2), label="Output", linewidth=5
+    )[0]
+    lines[2] = curax.plot(
+        omeg * 1e-3, spec_3 / np.max(spec_3), label="Output", linewidth=5
+    )[0]
 
-    figmplf.suptitle('Spectrums with and without collisions')
-    plt.figlegend( lines, labels, loc = 'lower center', ncol=5, labelspacing=0. )
+    figmplf.suptitle("Spectrums with and without collisions")
+    plt.figlegend(lines, labels, loc="lower center", ncol=5, labelspacing=0.0)
 
-    plt.savefig('Collisions.png')
+    plt.savefig("Collisions.png")
 
-if __name__== '__main__':
 
+if __name__ == "__main__":
     main()

Examples/singleparticleacf.py
@@ -2,125 +2,190 @@
 """
 This will create images of the signal particle ACFs using the default gordyev plugin
 """
+
 from pathlib import Path
-import numpy as np
-import scipy.fftpack as fftsy
-import scipy.special
-import scipy.constants as spconst
+
 import matplotlib.pylab as plt
+import numpy as np
+import scipy.constants as spconst
+import scipy.fftpack as fftsy
+import scipy.special as sp_spec
 from matplotlib import rc
+from matplotlib.markers import MarkerStyle
+
 #
-from ISRSpectrum.plugins.gord_default import magacf, collacf, magncollacf
-
-
+from ISRSpectrum.plugins.gord_default import (
+    collacf,
+    magacf,
+    magncollacf,
+    simpacf,
+    sommerfelderfrep,
+)
 
 
 def singleparticalacfs():
-    #%% Sim set up
-    centerFrequency = 440.2*1e6
-    nspec=129
-    sampfreq=50e3
+    # %% Sim set up
+    centerFrequency = 440.2 * 1e6
+    nspec = 1024
+    sampfreq = 50e3
     bMag = 0.4e-4
     Ts = 1e3
-    Partdict = {0:('Electron',spconst.m_e,spconst.e),1:('H+ Ion',spconst.m_p,spconst.e),2:('O+ Ion',16*spconst.m_p,spconst.e)}
-    particle = Partdict[0]
+    Partdict = {
+        0: ("Electron", spconst.m_e, spconst.e),
+        1: ("H+ Ion", spconst.m_p, spconst.e),
+        2: ("O+ Ion", 16 * spconst.m_p, spconst.e),
+    }
+    curpart = 1
+    particle = Partdict[curpart]
     pname = particle[0]
 
     ms = particle[1]
     q_ch = particle[2]
-    K = 2.0*np.pi*2*centerFrequency/spconst.c
-    f = np.arange(-np.ceil((nspec-1.0)/2.0),np.floor((nspec-1.0)/2.0+1))*(sampfreq/(2*np.ceil((nspec-1.0)/2.0)))
-    C = np.sqrt(spconst.k*Ts/ms)
-    Om = q_ch*bMag/ms
+    K = 2.0 * np.pi * 2 * centerFrequency / spconst.c
 
-    omeg_s = 2.0*np.pi*f
-    theta = omeg_s/(K*C*np.sqrt(2.0))
+    f = np.arange(-np.ceil((nspec - 1.0) / 2.0), np.floor((nspec - 1.0) / 2.0 + 1)) * (
+        sampfreq / (2 * np.ceil((nspec - 1.0) / 2.0))
+    )
+    C = np.sqrt(spconst.k * Ts / ms)
+    Om = q_ch * bMag / ms / (K * C * np.sqrt(2))
 
-    dtau = 2e-2*2.0/(K*C*np.sqrt(2.0))
+    omeg_s = 2.0 * np.pi * f
+    theta = omeg_s / (K * C * np.sqrt(2.0))
+
+    dtau = 2e-3
     N = 2**12
-    tau = np.arange(N)*dtau
-    d2r = np.pi/180.0
+    N_somm = 2**7
+    b1 = N_somm * dtau
+    tau = np.arange(N) * dtau
+    d2r = np.pi / 180.0
 
-#%% No collisions or magnetic field
-    gordnn = np.exp(-np.power(C*K*tau,2.0)/2.0)
+    # %% No collisions or magnetic field
+    gordnn = np.exp(-np.power(tau, 2.0) / 4.0)
+
+    fig1 = plt.figure()
+    plt.plot(tau, gordnn, linewidth=3)
+    plt.title(r"Single Particle ACF for " + pname)
+    plt.grid(True)
+    plt.savefig("ACF" + pname.replace(" ", "") + ".png")
+    plt.close(fig1)
+    num_g = np.sqrt(np.pi) * np.exp(-(theta**2)) - 1j * 2.0 * sp_spec.dawsn(theta)
+    den_g = K * C * np.sqrt(2)
+    gord = num_g / den_g
+
+    (Gn, flag_c, outrep) = sommerfelderfrep(
+        simpacf, N_somm, theta, b1, Lmax=500, errF=1e-7
+    )
+    gordn = Gn / den_g
+
+    f_khz = f * 1e-3
+    fig2, ax = plt.subplots(1, 2)
+    ax[0].plot(f_khz, gord.real, label="analytic Real")
+    ax[1].plot(f_khz, gord.imag, label="analytic imag")
+    ax[0].plot(f_khz, gordn.real, label="numerical Real", linestyle="dashed")
+    ax[1].plot(f_khz, gordn.imag, label="numerical imag", linestyle="dashed")
+    ax[0].set_xlabel("Frequency kHz")
+    ax[1].set_xlabel("Frequency kHz")
+    ax[0].set_ylabel("Ampltiude")
+
+    ax[0].set_title("Gordyev Real Part")
+    ax[1].set_title("Gordyev Imag Part")
+    ax[0].grid(True)
+    ax[1].grid(True)
+    fig2.tight_layout()
+    plt.savefig("gord" + pname.replace(" ", "") + ".png")
+    plt.close(fig2)
+
+    # %% With collisions
+    orig_vec = np.logspace(-5.0, -1, 5)
+    zvec = np.zeros_like(orig_vec)
+    nucoll_vec = np.concat([orig_vec, zvec, orig_vec], axis=0)
+    nuneu_vec = np.concat([zvec, orig_vec, orig_vec])
+
+    sp_acf = np.zeros((len(nuneu_vec), len(tau)))
+    alpha = np.pi / 2
+    for inum, (inuc, inuneu) in enumerate(zip(nucoll_vec, nuneu_vec)):
+        sp_acf[inum, :] = magncollacf(tau, K, C, alpha, Om, inuc / 2, inuc / 2, inuneu)
+    # np.exp(-np.power(K*C/numat,2.0)*(numat*taumat-1+np.exp(-numat*taumat)))
 
     plt.figure()
-    plt.plot(tau,gordnn,linewidth=3)
-    plt.title(r'Single Particle ACF for ' +pname)
-    plt.grid(True)
-    plt.savefig('ACF'+pname.replace(" ", "")+'.png')
-#%% With collisions
-    nuvec = np.logspace(-2.0,2.0,10)*K*C
+    plt.plot(
+        tau, sp_acf, linestyle="--", color="b", linewidth=4, label=r"No Collisions"
+    )
 
-    taumat = np.tile(tau[np.newaxis,:],(len(nuvec),1))
-    numat = np.tile(nuvec[:,np.newaxis],(1,len(tau)))
-
-    gordnun = collacf(taumat,K,C,numat)
-    #np.exp(-np.power(K*C/numat,2.0)*(numat*taumat-1+np.exp(-numat*taumat)))
-
-    plt.figure()
-    plt.plot(tau,gordnn,linestyle='--',color='b',linewidth=4,label=r'No Collisions')
-
-    for inun, inu in enumerate(nuvec):
-        numult = inu/(K*C)
-        plt.plot(tau,gordnun[inun].real,linewidth=3,label=r'$\nu = {:.2f} KC$'.format(numult))
+    for inun, (inuc, inuneu) in enumerate(zip(nucoll_vec, nuneu_vec)):
+        curlabel = r"$\nu_c$ = {:.2f} KC, $\nu_n$ = {:.2f} KC".format(inuc, inuneu)
+        plt.plot(
+            tau,
+            sp_acf[inun].real,
+            linewidth=3,
+            label=curlabel,
+        )
 
     plt.grid(True)
-    plt.title(r'Single Particle ACF W/ Collisions for '+pname)
+    plt.title(r"Single Particle ACF W/ Collisions for " + pname)
     plt.legend()
-    plt.savefig('ACFwcolls'+pname.replace(" ", "")+'.png')
+    plt.savefig("ACFwcolls" + pname.replace(" ", "") + ".png")
 
-#%% With magnetic field
-    alpha = np.linspace(19,1,10)
-    taumat = np.tile(tau[np.newaxis,:],(len(alpha),1))
-    almat = np.tile(alpha[:,np.newaxis],(1,len(tau)))
-    Kpar = np.sin(d2r*almat)*K
-    Kperp = np.cos(d2r*almat)*K
+    # %% With magnetic field
+    alpha = np.linspace(19, 1, 10)
+    taumat = np.tile(tau[np.newaxis, :], (len(alpha), 1))
+    almat = np.tile(alpha[:, np.newaxis], (1, len(tau)))
+    Kpar = np.sin(d2r * almat) * K
+    Kperp = np.cos(d2r * almat) * K
 
-#    gordmag = np.exp(-np.power(C*Kpar*taumat,2.0)/2.0-2.0*np.power(Kperp*C*np.sin(Om*taumat/2.0)/Om,2.0))
-    gordmag = magacf(taumat,K,C,d2r*almat,Om)
+    #    gordmag = np.exp(-np.power(C*Kpar*taumat,2.0)/2.0-2.0*np.power(Kperp*C*np.sin(Om*taumat/2.0)/Om,2.0))
+    gordmag = magacf(taumat, K, C, d2r * almat, Om)
     plt.figure()
-    plt.plot(tau,gordnn,linestyle='--',color='b',linewidth=4,label='No B-field')
+    plt.plot(tau, gordnn, linestyle="--", color="b", linewidth=4, label="No B-field")
 
     for ialn, ial in enumerate(alpha):
-        plt.plot(tau,gordmag[ialn].real,linewidth=3,label=r'$\alpha = {:.0f}^\circ$'.format(ial))
+        plt.plot(
+            tau,
+            gordmag[ialn].real,
+            linewidth=3,
+            label=r"$\alpha = {:.0f}^\circ$".format(ial),
+        )
 
     plt.grid(True)
-    plt.title('Single Particle ACF W/ Mag for ' +pname)
+    plt.title("Single Particle ACF W/ Mag for " + pname)
     plt.legend()
-    plt.savefig('ACFwmag'+pname.replace(" ", "")+'.png')
+    plt.savefig("ACFwmag" + pname.replace(" ", "") + ".png")
+
+    # # %% Error surface with both
+    # almat3d = np.tile(alpha[:, np.newaxis, np.newaxis], (1, len(nuvec), len(tau)))
+    # numat3d = np.tile(nuvec[np.newaxis, :, np.newaxis], (len(alpha), 1, len(tau)))
+    # taumat3d = np.tile(tau[np.newaxis, np.newaxis, :], (len(alpha), len(nuvec), 1))
+    # Kpar3d = np.sin(d2r * almat3d) * K
+    # Kperp3d = np.cos(d2r * almat3d) * K
+    # gam = np.arctan(numat3d / Om)
+
+    #    deltl = np.exp(-np.power(Kpar3d*C/numat3d,2.0)*(numat3d*taumat3d-1+np.exp(-numat3d*taumat3d)))
+    #    deltp = np.exp(-np.power(C*Kperp3d,2.0)/(Om*Om+numat3d*numat3d)*(np.cos(2*gam)+numat3d*taumat3d-np.exp(-numat3d*taumat3d)*(np.cos(Om*taumat3d-2.0*gam))))
+
+    # #    gordall = deltl*deltp
+    # gordall = magncollacf(taumat3d, K, C, d2r * almat3d, Om, numat3d)
+    # gordnnmat = np.tile(gordnn[np.newaxis, np.newaxis, :], (len(alpha), len(nuvec), 1))
+
+    # gorddiff = np.abs(gordall - gordnnmat) ** 2
+    # err = np.sqrt(gorddiff.mean(2)) / np.sqrt(np.power(gordnn, 2.0).sum())
+    # extent = [
+    #     np.log10(nuvec[0] / (K * C)),
+    #     np.log10(nuvec[-1] / (K * C)),
+    #     alpha[0],
+    #     alpha[-1],
+    # ]
+
+    # plt.figure()
+    # myim = plt.imshow(err * 100, extent=extent, origin="lower", aspect="auto")
+    # myim.set_clim(0.0, 5.0)
+    # plt.xlabel(r"$\log_{10}(\nu /KC)$")
+    # plt.ylabel(r"$^\circ\alpha$")
+    # cbar = plt.colorbar()
+    # cbar.set_label("% Error", rotation=270)
+    # cbar.ax.get_yaxis().labelpad = 15
+    # plt.title("Error between ideal ACF and with Collisions and B-field for " + pname)
+    # plt.savefig("ACFerr" + pname.replace(" ", "") + ".png")
 
 
-#%% Error surface with both
-    almat3d = np.tile(alpha[:,np.newaxis,np.newaxis],(1,len(nuvec),len(tau)))
-    numat3d = np.tile(nuvec[np.newaxis,:,np.newaxis],(len(alpha),1,len(tau)))
-    taumat3d = np.tile(tau[np.newaxis,np.newaxis,:],(len(alpha),len(nuvec),1))
-    Kpar3d = np.sin(d2r*almat3d)*K
-    Kperp3d = np.cos(d2r*almat3d)*K
-    gam = np.arctan(numat3d/Om)
-
-#    deltl = np.exp(-np.power(Kpar3d*C/numat3d,2.0)*(numat3d*taumat3d-1+np.exp(-numat3d*taumat3d)))
-#    deltp = np.exp(-np.power(C*Kperp3d,2.0)/(Om*Om+numat3d*numat3d)*(np.cos(2*gam)+numat3d*taumat3d-np.exp(-numat3d*taumat3d)*(np.cos(Om*taumat3d-2.0*gam))))
-
-#    gordall = deltl*deltp
-    gordall = magncollacf(taumat3d,K,C,d2r*almat3d,Om,numat3d)
-    gordnnmat = np.tile(gordnn[np.newaxis,np.newaxis,:],(len(alpha),len(nuvec),1))
-
-    gorddiff = np.abs(gordall-gordnnmat)**2
-    err = np.sqrt(gorddiff.mean(2))/np.sqrt(np.power(gordnn,2.0).sum())
-    extent = [np.log10(nuvec[0]/(K*C)),np.log10(nuvec[-1]/(K*C)),alpha[0],alpha[-1]]
-
-    plt.figure()
-    myim = plt.imshow(err*100,extent = extent,origin='lower',aspect='auto')
-    myim.set_clim(0.0,5.)
-    plt.xlabel(r'$\log_{10}(\nu /KC)$')
-    plt.ylabel(r'$^\circ\alpha$')
-    cbar = plt.colorbar()
-    cbar.set_label('% Error', rotation=270)
-    cbar.ax.get_yaxis().labelpad = 15
-    plt.title('Error between ideal ACF and with Collisions and B-field for '+pname)
-    plt.savefig('ACFerr'+pname.replace(" ", "")+'.png')
-
-
-if __name__== '__main__':
+if __name__ == "__main__":
     singleparticalacfs()

Examples/sysspecs.py
@@ -3,11 +3,13 @@
 This script will create plots of ISR spectra for various radar systems. The ion acoustic frequencies will also be plotted.
 
 """
+
+import matplotlib.pylab as plt
 import numpy as np
 import scipy as sp
 import scipy.constants as spconst
-import matplotlib.pylab as plt
 import seaborn as sns
+
 sns.set_style("whitegrid")
 sns.set_context("notebook")
 #
@@ -15,42 +17,63 @@ import ISRSpectrum as ISSnew
 
 
 def main():
-
     Ne = 1e11
     Ti = 3e3
     Te = 3e3
-    databloc =  np.array([[Ne,Ti],[Ne,Te]])
-    species = ['O+','e-']
+    databloc = np.array([[Ne, Ti], [Ne, Te]])
+    species = ["O+", "e-"]
 
-    Cia = np.sqrt(spconst.k*(Te+3.*Ti)/16./spconst.m_p)
+    Cia = np.sqrt(spconst.k * (Te + 3.0 * Ti) / 16.0 / spconst.m_p)
 
-    #make list of dictionaries for the radar systems
-    biglist = [{'name':'AMISR','Fo':449e6,'Fs':50e3,'alpha':70.},
-        {'name':'Sondrestrom','Fo':1290e6,'Fs':100e3,'alpha':80.},
-        {'name':'Haystack','Fo':440e6,'Fs':50e3,'alpha':65.},
-        {'name':'Arecibo','Fo':430e6,'Fs':50e3,'alpha':45.},
-        {'name':'Jicamarca','Fo':50e6,'Fs':10e3,'alpha':1.}]
+    # make list of dictionaries for the radar systems
+    biglist = [
+        {"name": "AMISR", "Fo": 449e6, "Fs": 50e3, "alpha": 70.0},
+        {"name": "Sondrestrom", "Fo": 1290e6, "Fs": 100e3, "alpha": 80.0},
+        {"name": "Haystack", "Fo": 440e6, "Fs": 50e3, "alpha": 65.0},
+        {"name": "Arecibo", "Fo": 430e6, "Fs": 50e3, "alpha": 45.0},
+        {"name": "Jicamarca", "Fo": 50e6, "Fs": 10e3, "alpha": 1.0},
+    ]
 
-    (figmplf, axmat) = plt.subplots(3, 2,figsize=(20, 15), facecolor='w')
+    (figmplf, axmat) = plt.subplots(3, 2, figsize=(20, 15), facecolor="w")
     axvec = axmat.flatten()
-    lines = [None]*2
-    labels = ['Spectrum','Ion Acoustic Frequency']
-    for ima,idict in enumerate(biglist):
+    lines = [None] * 2
+    labels = ["Spectrum", "Ion Acoustic Frequency"]
+    for ima, idict in enumerate(biglist):
         curax = axvec[ima]
-        k = 2.0*np.pi*2*idict['Fo']/spconst.c
-        xloc = np.array([-k*Cia,k*Cia])/2/np.pi/2
-        ISS1 = ISSnew.Specinit(centerFrequency = idict['Fo'], bMag = 0.4e-4, nspec=256, sampfreq=idict['Fs'],dFlag=True)
-        (omeg,spec)=ISS1.getspecsep(databloc,species,vel = 0.0, alphadeg=idict['alpha'],rcsflag=False)
+        k = 2.0 * np.pi * 2 * idict["Fo"] / spconst.c
+        xloc = np.array([-k * Cia, k * Cia]) / 2 / np.pi / 2
+        ISS1 = ISSnew.Specinit(
+            centerFrequency=idict["Fo"],
+            bMag=0.4e-4,
+            nspec=512,
+            sampfreq=idict["Fs"],
+            dFlag=True,
+        )
+        (omeg, spec) = ISS1.getspecsep(
+            databloc, species, vel=0.0, alphadeg=idict["alpha"], rcsflag=False
+        )
 
-        lines[0] = curax.plot(omeg*1e-3,spec,label='Output',linewidth=5)[0]
-        lines[1] = curax.stem(xloc*1e-3, np.ones(2)*np.amax(spec), linefmt='g--', markerfmt='go', basefmt=' ')[0]
-        curax.set_xlabel('f in kHz')
-        curax.set_ylabel('Amp')
-        curax.set_title(idict['name']+ ' Spectrtum')
-    figmplf.suptitle(r'Spectrums $N_e$ = {0:.1e}m$^{{-3}}$, $T_e$ = {1:.0f}$^o$K, $T_i$ = {2:.0f}$^o$K'.format(Ne,Te,Ti), fontsize=20)
-    plt.figlegend( lines, labels, loc = 'lower center', ncol=5, labelspacing=0. )
+        lines[0] = curax.plot(omeg * 1e-3, spec, label="Output", linewidth=5)[0]
+        lines[1] = curax.stem(
+            xloc * 1e-3,
+            np.ones(2) * np.amax(spec),
+            linefmt="g--",
+            markerfmt="go",
+            basefmt=" ",
+        )[0]
+        curax.set_xlabel("f in kHz")
+        curax.set_ylabel("Amp")
+        curax.set_title(idict["name"] + " Spectrtum")
+    figmplf.suptitle(
+        r"Spectrums $N_e$ = {0:.1e}m$^{{-3}}$, $T_e$ = {1:.0f}$^o$K, $T_i$ = {2:.0f}$^o$K".format(
+            Ne, Te, Ti
+        ),
+        fontsize=20,
+    )
+    plt.figlegend(lines, labels, loc="lower center", ncol=5, labelspacing=0.0)
 
-    plt.savefig('DifferentSystems.png')
-if __name__== '__main__':
+    plt.savefig("DifferentSystems.png")
 
+
+if __name__ == "__main__":
     main()

ISRSpectrum/ISRSpectrum.py
@@ -11,11 +11,13 @@ E. Kudeki and M. A. Milla, 2011.
 The intent of the code is to be able to calculate an ISR spectrum in a number of
 different conditions except for a very low magnetic aspect angles (<1deg).
 """
-from six import string_types
+
 import numpy as np
 import scipy.constants as spconst
+from six import string_types
+
+from .collision_calc import INFODICT, get_collisionfreqs, getionmass
 from .plugins import gordplugs
-from .collision_calc import get_collisionfreqs, getionmass, getionmass, INFODICT
 
 
 class Specinit(object):
@@ -47,7 +49,7 @@ class Specinit(object):
         bMag=0.4e-4,
         nspec=64,
         sampfreq=50e3,
-        collfreqmin=1e-2,
+        collfreqmin=1e-6,
         alphamax=30.0,
         dFlag=False,
         f=None,
@@ -105,7 +107,7 @@ class Specinit(object):
         heflag=False,
         seplines=False,
         *args,
-        **kwargs
+        **kwargs,
     ):
         """Gives the spectrum and the frequency vectors.
 
@@ -170,6 +172,8 @@ class Specinit(object):
         # normalize total ion density to be the same as electron density
         ionstuff[:, 0] = (estuff[0] / ionden) * ionstuff[:, 0]
 
+        ikeep = ionstuff[:, 0] / estuff[0] > 1e-4
+        ionstuff = ionstuff[ikeep]
         firstion = True
         wevec = np.zeros(Nions)
         Tivec = np.zeros(Nions)
@@ -284,12 +288,12 @@ class Specinit(object):
         """
 
         assert Islistofstr(ionspecies), "Species needs to be a list of strings"
-        assert allin(
-            ionspecies, list(INFODICT.keys())
-        ), "Have unnamed species in the list."
-        assert len(ionspecies) == len(
-            ionfracs
-        ), "Length of species names and fractions need to be same length"
+        assert allin(ionspecies, list(INFODICT.keys())), (
+            "Have unnamed species in the list."
+        )
+        assert len(ionspecies) == len(ionfracs), (
+            "Length of species names and fractions need to be same length"
+        )
 
         nspec = len(ionspecies) + 1
         datablock = np.zeros((nspec, 2))
@@ -356,13 +360,13 @@ class Specinit(object):
             The scaled density needed to calculate the RCS in m^{-3}.
         """
         assert Islistofstr(species), "Species needs to be a list of strings"
-        assert allin(
-            species, list(INFODICT.keys())
-        ), "Have unnamed species in the list."
+        assert allin(species, list(INFODICT.keys())), (
+            "Have unnamed species in the list."
+        )
         nspec = datablock.shape[0]
-        datablocknew = np.zeros((nspec, 7))
+        datablocknew = np.zeros((nspec, 8))
 
-        (nuparr, nuperp) = get_collisionfreqs(
+        (nuparr, nuperp, nuneu) = get_collisionfreqs(
             datablock, species, n_datablock=n_datablock, n_species=n_species
         )
         for nspec, ispec in enumerate(species):
@@ -372,6 +376,7 @@ class Specinit(object):
             if col_calc:
                 datablocknew[nspec, 5] = nuparr[nspec]
                 datablocknew[nspec, 6] = nuparr[nspec]
+                datablocknew[nspec, 7] = nuneu[nspec]
 
         return self.getspec(
             datablocknew,

ISRSpectrum/collision_calc.py
@@ -4,8 +4,10 @@ collision_calc.py
 
 This module is used to calculate collision frequencies.
 """
-import numpy as np
+
 from pathlib import Path
+
+import numpy as np
 import pandas as pd
 
 # %% Collision frequencies
@@ -95,11 +97,15 @@ def get_collisionfreqs(
         A Nsp length numpy array that holds the parrallel collision frequencies in s^-1.
     nuperp : ndarray
         A Nsp length numpy array that holds the perpendictular collision frequencies in s^-1.
+    nuneu : ndarray
+        A Nsp length numpy array that holds the neutral collision frequencies in s^-1.
     """
 
     nuperp = np.zeros((datablock.shape[0]))
     nuparr = np.zeros_like(nuperp)
+    nuneu = np.zeros_like(nuperp)
 
+    # Translate densities to cm^-3 from m^-3 since all the constants are in cgs units.
     Ni = datablock[:-1, 0] * 1e-6
     Ti = datablock[:-1, 1]
     Ne = datablock[-1, 0] * 1e-6
@@ -124,7 +130,6 @@ def get_collisionfreqs(
     # ionion collisions
     for si, s in enumerate(species[:-1]):
         for ti, t in enumerate(species[:-1]):
-
             nuparr[si] = nuparr[si] + Bst[s][t] * Ni[ti] / np.power(Ti[ti], 1.5)
 
     if (n_datablock is not None) and (n_species is not None):
@@ -133,24 +138,21 @@ def get_collisionfreqs(
         # ion neutral collisions
         for si, s in enumerate(species[:-1]):
             for ti, t in enumerate(n_species):
-
                 curCin = Cin[s][t]
 
                 if np.isnan(curCin):
                     neuts = r_ion_neutral(s, t, Ni[si], Nn[ti], Ti[si], Tn[ti])
                 else:
-                    neuts = curCin * Ni[si]
-                nuparr[si] = nuparr[si] + neuts
+                    neuts = curCin * Nn[ti] * 1e-10
+                nuneu[si] += neuts
         # electron neutral collision frequencies
         for ti, t in enumerate(n_species):
-            nueneu = e_neutral(t, Nn[ti], Te)
-            nuperp[-1] = nuperp[-1] + nueneu
-            nuparr[-1] = nuparr[-1] + nueneu
+            nuneu[-1] += e_neutral(t, Nn[ti], Te)
 
     # according to milla and kudeki the collision for perpendicular and parrallel
     # directions only matter for the electrons
     nuperp[:-1] = nuparr[:-1]
-    return (nuparr, nuperp)
+    return (nuparr, nuperp, nuneu)
 
 
 def r_ion_neutral(s, t, Ni, Nn, Ti, Tn):
@@ -194,7 +196,6 @@ def r_ion_neutral(s, t, Ni, Nn, Ti, Tn):
     A = nudict[sp1][0]
     B = nudict[sp1][1]
     if sp1 == ("O+", "H"):
-
         nu_ineu = A * Nn * np.power(Ti / 16.0 + Tn, 0.5)
     elif sp1 == ("H+", "O"):
         nu_ineu = A * Nn * np.power(Ti, 0.5) * (1 - B * np.log10(Ti)) ** 2
@@ -204,8 +205,7 @@ def r_ion_neutral(s, t, Ni, Nn, Ti, Tn):
 
 
 def e_neutral(t, Nn, Te):
-    """This will calculate electron - neutral reactions collision frequencies. See
-    table 4.6 in Schunk and Nagy.
+    """This will calculate electron - neutral reactions collision frequencies. See table 4.6 in Schunk and Nagy.
 
     Parameters
     ----------

ISRSpectrum/plugins/gord_default.py
@@ -4,18 +4,15 @@
 This module will calcualte the Gordeyve integral for various cases, with collisions, with a magnetic field up to .1 degrees off of perp to B. This cut is controlled by an assert statement.
 """
 
-
 import numpy as np
-import scipy.special as sp_spec
 import scipy.constants as spconst
-import numpy as np
 import scipy.fftpack as fftsy
+import scipy.special as sp_spec
 
 
 class GordPlug:
-
     def calcgordeyev(
-        self, dataline, alpha, K, omeg, bMag, collfreqmin=1e-2, alphamax=30, dFlag=False
+        self, dataline, alpha, K, omeg, bMag, collfreqmin=1e-6, alphamax=30, dFlag=False
     ):
         """
         Performs the Gordeyve integral calculation for main cases from the first Kudeki/Milla paper.
@@ -30,7 +27,9 @@ class GordPlug:
                 Vs - The Doppler velocity in m/s.
                 qs - The charge of the species in elementary charges. (Value will be replaced for the electrons)
                 ms - Mass of the species in AMU. (Value will be replaced for the electrons)
-                nus - Collision frequency for species in s^-1.
+                nuparr - Parallel collision frequency for species in s^-1.
+                nuperp - Perpendicular collision frequency for species in s^-1
+                nuneu - Neutral collision frequency for species in s^-1
         alphadeg : float
             The magnetic aspect angle in radians.
         K : float
@@ -60,71 +59,86 @@ class GordPlug:
             An array of the Doppler corrected radian frequency
         """
 
-        assert (
-            alpha > 0.1
-        ), "Angle off of perp to B must be greater than .1 degrees otherwise integral will not converge."
-        assert (
-            len(dataline) >= 6
-        ), "The dataline input needs to be length of at least 6 elements."
-        (Ns, Ts, Vs, qs, ms, nus) = dataline[:6]
+        assert alpha > 0.1 * np.pi / 180.0, (
+            "Angle off of perp to B must be greater than .1 degrees otherwise integral will not converge."
+        )
+        assert len(dataline) >= 8, (
+            "The dataline input needs to be length of at least 8 elements."
+        )
+        (Ns, Ts, Vs, qs, ms, nuparr, nuperp, nuneu) = dataline
 
+        # if ms == 16:
+        #     import ipdb
+
+        #     ipdb.set_trace()
         if qs < 0:
             qs = -spconst.e
             ms = spconst.m_e
         else:
             qs = qs * spconst.e
             ms = ms * spconst.m_p
-
-        nur = np.pi * 2 * nus
+        # Thermal Speed
         C = np.sqrt(spconst.k * Ts / ms)
+        # Normalization Term
+        o_norm = K * C * np.sqrt(2)
+        nuparr_n = nuparr / o_norm
+        nuperp_n = nuperp / o_norm
+        nuneu_n = nuneu / o_norm
+
         omeg_s = omeg - K * Vs
         theta = omeg_s / (K * C * np.sqrt(2.0))
         Om = qs * bMag / ms
+        Om_n = Om / o_norm
         # determine what integral is used
         magbool = alpha * 180.0 / np.pi < alphamax
-        collbool = collfreqmin * C * K < nur
+        # colminrad = collfreqmin * C * K
+        collbool = (
+            (nuparr_n + nuperp_n) > collfreqmin
+        )  # colminrad < nuparr_r or colminrad < nuperp_r or colminrad < nuneu_r
 
+        # if Ts > 1500:
+        #     import ipdb
+
+        #     ipdb.set_trace()
         if not collbool and not magbool:
             # for case with no collisions or magnetic field just use analytic method
             num_g = np.sqrt(np.pi) * np.exp(-(theta**2)) - 1j * 2.0 * sp_spec.dawsn(
                 theta
             )
-            den_g = K * C * np.sqrt(2)
+            den_g = o_norm
             gord = num_g / den_g
 
             if dFlag:
                 print("\t No collisions No magnetic field,again")
             return (gord, Ts, Ns, qs, omeg_s)
 
-        elif collbool and not magbool:
-            if dFlag:
-                print("\t With collisions No magnetic field")
-            gordfunc = collacf
-            exparams = (K, C, nur)
-        elif not collbool and magbool:
-            if dFlag:
-                print("\t No collisions with magnetic field")
-            gordfunc = magacf
-            exparams = (K, C, alpha, Om)
+        # elif collbool and not magbool:
+        #     if dFlag:
+        #         print("\t With collisions No magnetic field")
+        #     gordfunc = collacf
+        #     exparams = (nuparr_n, nuperp_n, nuneu_n)
+        # elif not collbool and magbool:
+        #     if dFlag:
+        #         print("\t No collisions with magnetic field")
+        #     gordfunc = magacf
+        #     exparams = (alpha, Om_n)
         else:
             if dFlag:
                 print("\t With collisions with magnetic field")
             gordfunc = magncollacf
-            exparams = (K, C, alpha, Om, nur)
+            exparams = (alpha, Om_n, nuparr_n, nuperp_n, nuneu_n)
 
-        maxf = np.abs(omeg / (2 * np.pi)).max()
+        maxf = np.abs(theta / (2 * np.pi)).max()
         T_s = 1.0 / (2.0 * maxf)
 
-        #        N_somm = 2**15
-        #        b1 = 10.0/(K*C*np.sqrt(2.0))
-        # changed ot sample grid better
-        N_somm = 2**10
+        N_somm = 100
         b1 = T_s * N_somm
-        #        b1 = interval/10.
-        #        N_somm=np.minimum(2**10,np.ceil(b1/T_s))
-        (gord, flag_c, outrep) = sommerfelderfrep(
-            gordfunc, N_somm, omeg_s, b1, Lmax=500, errF=1e-7, exparams=exparams
+
+        (Gn, flag_c, outrep) = sommerfelderfrep(
+            gordfunc, N_somm, theta, b1, Lmax=500, errF=1e-6, exparams=exparams
         )
+        # If there are neutral collision it is neccesary to to normalize by (1-nuneu*Gn) from Milla and Kudeki's 2009 paper
+        gord = Gn / (1 - nuneu_n * Gn) / o_norm
         if dFlag:
             yna = ["No", "Yes"]
             print(
@@ -136,97 +150,124 @@ class GordPlug:
         return (gord, Ts, Ns, qs, omeg_s)
 
 
-def magacf(tau, K, C, alpha, Om):
+def simpacf(tt):
+    """Single particle ACF for case with no collisions and magnetic field. Used to make sure numerical integration is correct.
+
+    Parameters
+    ----------
+    tt : ndarray
+        The time vector for the acf in sqrt(2)*K*C seconds.
+
+    Returns
+    -------
+    spacf : ndarray
+        The single particle acf.
+    """
+    return np.exp(-(tt**2) / 4.0)
+
+
+def magacf(tt, alpha, Om):
     """Create a single particle acf for a species with magnetic field but no collisions.
 
     Parameters
     ----------
-    tau : ndarray
-        The time vector for the acf.
-    K : float
-        Bragg scatter vector magnetude.
-    C : float
-        Thermal speed of the species.
+    tt : ndarray
+        The time vector for the acf in sqrt(2)*K*C seconds.
     alpha : float
         Magnetic aspect angle in radians.
     Om : float
-        The gyrofrequency of the particle.
+        The gyrofrequency of the particle normalized by sqrt(2)*K*C.
 
     Returns
     -------
     spacf : ndarray
         The single particle acf.
     """
-    Kpar = np.sin(alpha) * K
-    Kperp = np.cos(alpha) * K
+    par = np.sin(alpha)
+    perp = np.cos(alpha)
 
-    par_ex = -np.power(C * Kpar * tau, 2.0) / 2.0
-    perp_ex = -2.0 * np.power((Kperp * C * np.sin(Om * tau / 2.0)) / Om, 2.0)
+    par_ex = -np.power(par * tt, 2.0) / 4.0
+    perp_ex = -1.0 * np.power((perp * np.sin(Om * tt / 2.0)) / Om, 2.0)
     spacf = np.exp(par_ex + perp_ex)
     return spacf
 
 
-def collacf(tau, K, C, nu):
+def collacf(tt, nuparr, nuperp, nuneu):
     """Create a single particle acf for a species with collisions, no magnetic field. See equation 48 in Kudeki and milla 2011.
 
     Parameters
     ----------
-    tau : ndarray
-        The time vector for the acf.
-    K : float
-        Bragg scatter vector magnetude.
-    C : float
-        Thermal speed of the species.
-    nu : float
-        The collision frequency in collisions*radians/sec
+    tt : ndarray
+        The time vector for the acf in sqrt(2)*K*C seconds.
+    nuparr : float
+        The parallel collision frequency normalized by sqrt(2)*K*C
+    nuperp : float
+        The perpendicular collision frequency normalized by sqrt(2)*K*C
+    nuneu : float
+        The neutral collision frequency normalized by sqrt(2)*K*C
 
     Returns
     -------
     spacf : ndarray
         The single particle acf.
     """
-    spacf = np.exp(-np.power(K * C / nu, 2.0) * (nu * tau - 1 + np.exp(-nu * tau)))
-    return spacf
+
+    if nuparr + nuperp > 1e-6:
+        spacf = np.exp(
+            -2.0 * (nuparr * tt - 1 + np.exp(-nuparr * tt)) / np.power(nuparr, 2.0)
+        )
+    else:
+        spacf = np.exp(-np.power(tt, 2) / 4)
+
+    neu_acf = np.exp(-nuneu * tt)
+    return spacf * neu_acf
 
 
-def magncollacf(tau, K, C, alpha, Om, nu):
+def magncollacf(tt, alpha, Om, nuparr, nuperp, nuneu):
     """Create a single particle acf for a species with magnetic field and collisions.
 
     Parameters
     ----------
-    tau : ndarray
-        The time vector for the acf.
-    K : float
-        Bragg scatter vector magnetude.
-    C : float
-        Thermal speed of the species.
+    tt : ndarray
+        The time vector for the acf in sqrt(2)*K*C seconds.
     alpha : float
         Magnetic aspect angle in radians.
     Om : float
-        The gyrofrequency of the particle.
-    nu : float
-        The collision frequency in collisions*radians/sec
+        The gyrofrequency of the particle normalized by sqrt(2)*K*C.
+    nuparr : float
+        The parallel collision frequency normalized by sqrt(2)*K*C
+    nuperp : float
+        The perpendicular collision frequency normalized by sqrt(2)*K*C
+    nuneu : float
+        The neutral collision frequency normalized by sqrt(2)*K*C
 
     Returns
     -------
     acf : ndarray
         The single particle acf.
     """
-    Kpar = np.sin(alpha) * K
-    Kperp = np.cos(alpha) * K
-    gam = np.arctan(nu / Om)
-
-    deltl = np.exp(-np.power(Kpar * C / nu, 2.0) * (nu * tau - 1 + np.exp(-nu * tau)))
-    deltp = np.exp(
-        -np.power(C * Kperp, 2.0)
-        / (Om * Om + nu * nu)
-        * (
-            np.cos(2 * gam)
-            + nu * tau
-            - np.exp(-nu * tau) * (np.cos(Om * tau - 2.0 * gam))
+    par = np.sin(alpha)
+    perp = np.cos(alpha)
+    gam = np.arctan(nuperp / Om)
+    neu_acf = np.exp(-nuneu * tt)
+    if nuparr + nuperp > 1e-6:
+        deltl = np.exp(
+            -np.power(par / nuparr, 2.0) * (nuparr * tt - 1.0 + np.exp(-nuparr * tt))
         )
-    )
-    return deltl * deltp
+        deltp = np.exp(
+            -np.power(perp, 2.0)
+            / (Om**2 + nuperp**2)
+            * (
+                np.cos(2 * gam)
+                + nuperp * tt
+                - np.exp(-nuperp * tt) * (np.cos(Om * tt - 2.0 * gam))
+            )
+        )
+    else:
+        deltl = np.exp(-np.power(par * tt, 2.0) / 4.0)
+        deltp = np.exp(-1.0 * np.power((perp * np.sin(Om * tt / 2.0)) / Om, 2.0))
+
+    return deltl * deltp * neu_acf
 
 
 def chirpz(Xn, A, W, M):
@@ -336,7 +377,6 @@ def sommerfeldchirpz(
     flag_c = False
 
     for irep in range(Lmax):
-
         fk = func(k + N * dk * irep, *exparams)
         Xkold = Xk
         Xk = chirpz(fk * wk, A_0, W_0, M) * np.power(W_0, N * dk * irep * freqm) + Xk
@@ -388,8 +428,8 @@ def sommerfelderfrep(func, N, omega, b1, Lmax=1, errF=0.1, exparams=()):
     """
     Xk = np.zeros_like(omega) * (1 + 1j)
     flag_c = False
-    for irep in range(Lmax):
 
+    for irep in range(Lmax):
         Xktemp = sommerfelderf(func, N, omega, b1 * irep, b1 * (irep + 1), exparams)
         Xkdiff = Xktemp.real**2 + Xktemp.imag**2
         Xk = Xk + Xktemp
@@ -399,6 +439,7 @@ def sommerfelderfrep(func, N, omega, b1, Lmax=1, errF=0.1, exparams=()):
         #        Xk = Xk+Xktemp
         outrep = irep + 1
         # check for convergence
+
         if np.sum(Xkdiff / Xkpow) < errF:
             flag_c = True
             outrep = irep

ISRSpectrum/plugins/mathutils.py
@@ -3,6 +3,7 @@
 """
 A number of mathematical tools needed to calculate the spectra.
 """
+
 import numpy as np
 import scipy.fftpack as fftsy
 import scipy.special
@@ -115,7 +116,6 @@ def sommerfeldchirpz(
     flag_c = False
 
     for irep in range(Lmax):
-
         fk = func(k + N * dk * irep, *exparams)
         Xkold = Xk
         Xk = chirpz(fk * wk, A_0, W_0, M) * np.power(W_0, N * dk * irep * freqm) + Xk
@@ -132,7 +132,7 @@ def sommerfeldchirpz(
     return (Xk, flag_c, outrep)
 
 
-def sommerfelderfrep(func, N, omega, b1, Lmax=1, errF=0.1, exparams=()):
+def sommerfelderfrep(func, N, omega, b1, Lmax=100, errF=1e-6, exparams=()):
     """Numerically integrate Sommerfeld like integral using erf transform function loop.
 
     This function will numerically integrate a Sommerfeld like integral, int(exp(-jwk)f(k),k=0..inf) using the ERF transform and 2N+1 samples and at most Lmax loops. If the normalized difference between the previous estimate of the output Xk is less then the parameter errF then the loop stops and a flag that represents convergence is set to true. A number of loops is also output as well.
@@ -168,7 +168,6 @@ def sommerfelderfrep(func, N, omega, b1, Lmax=1, errF=0.1, exparams=()):
     Xk = np.zeros_like(omega) * (1 + 1j)
     flag_c = False
     for irep in range(Lmax):
-
         Xktemp = sommerfelderf(func, N, omega, b1 * irep, b1 * (irep + 1), exparams)
         Xkdiff = Xktemp.real**2 + Xktemp.imag**2
         Xk = Xk + Xktemp
@@ -201,7 +200,7 @@ def sommerfelderf(func, N, omega, a, b, exparams=()):
     a : float
         Lower bound of the integral.
     b : float
-        Upper bound of teh integral.
+        Upper bound of the integral.
     exparams :tuple
         Any extra params other then k to create f(k), default ().
 